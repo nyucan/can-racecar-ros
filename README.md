f5f86ba012ad7a9b364e0f9c13579ed4d61acceb
Mingyu Zhao <mingyusysu@gmail.com>
2020-04-16 22:15:31 -0400

    finished visualizing the path

---
+++ README.md
@@ -0,0 +1,1 @@
+# CAN Lab Self Driving Car Project
---
+++ src/util.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import tf
+
+
+def quaternion_to_angle(q):
+    """Convert a quaternion _message_ into an angle in radians.
+    The angle represents the yaw.
+    This is not just the z component of the quaternion."""
+    x, y, z, w = q.x, q.y, q.z, q.w
+    roll, pitch, yaw = tf.transformations.euler_from_quaternion((x, y, z, w))
+    return yaw
